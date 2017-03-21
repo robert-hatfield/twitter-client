9d5d7b681ecb552b4632fcf6a2ac792dbfebe03b
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-03-21 13:17:18 -0700

    Define getOAuthUser method in API singleton

--- TwitterClient/TwitterClient/API.swift
+++ TwitterClient/TwitterClient/API.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Accounts
+import Social
 
 typealias AccountCallback = (ACAccount?) -> ()
 typealias UserCallback = (User?) -> ()
@@ -24,7 +25,6 @@ class API {
         let accountType = accountStore.accountType(withAccountTypeIdentifier: ACAccountTypeIdentifierTwitter)
         
         accountStore.requestAccessToAccounts(with: accountType, options: nil) { (success, error) in
-            
             if let error = error {
                 print("Error: \(error.localizedDescription)")
                 callback(nil)
@@ -44,6 +44,35 @@ class API {
     
     // Get OAuth user using Social Framework - validating with Twitter
     private func getOAuthUser(callback: @escaping UserCallback) {
+        let url = URL(string: "https://api.twitter.com/1.1/account/verify_credentials.json")
+        
+        if let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, url: url, parameters: nil) {
+            
+            request.account = self.account
+            
+            request.perform(handler: { (data, response, error) in
+                if let error = error {
+                    print ("Error: \(error.localizedDescription)")
+                    callback(nil)
+                    return
+                }
+                
+                guard let response = response else { callback(nil); return }
+                guard let data = data else { callback(nil); return }
+                
+                switch response.statusCode {
+                case 200...299:
+                    // danger zone below - to be refactored in lab assignment
+                    if let userJSON = try! JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        let user = User(json: userJSON)
+                        callback(user)
+                    }default:
+                    print("Error: response came back with statusCode: \(response.statusCode)")
+                    callback(nil)
+                }
+            })
+        }
+        
         
     }
     
