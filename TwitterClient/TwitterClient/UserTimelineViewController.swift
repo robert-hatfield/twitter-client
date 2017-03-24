05bf93e9ea521ceb7bc10d9757c9630d554fdbe5
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-03-23 19:44:57 -0700

    Begin setup to show another user's timeline

---
+++ TwitterClient/TwitterClient/UserTimelineViewController.swift
@@ -0,0 +1,50 @@
+//
+//  UserTimelineViewController.swift
+//  TwitterClient
+//
+//  Created by Robert Hatfield on 3/23/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+import UIKit
+
+class UserTimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var allTweets = [Tweet]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        let tweetNib = UINib(nibName: "TweetNibCell", bundle: nil)
+        self.tableView.register(tweetNib, forCellReuseIdentifier: TweetNibCell.identifier)
+        
+        self.tableView.estimatedRowHeight = 50
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section: Int) -> Int { return allTweets.count }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
